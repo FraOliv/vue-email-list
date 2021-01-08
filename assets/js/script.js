fb3a2db57d803f1d089507902360fd4ba937a63c
FRANCESCO <OLIVOTTI>
2021-01-08 16:46:36 +0100

    almost finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-let app = new Vue({
-    el: "#app"
-})
+const app = new Vue({
+  el: "#app",
+  data: {
+    ListaMail: []
+  },
+  mounted: function() { 
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(risposta => {
+        this.ListaMail.push(risposta.data.response)
+     });
+    }
+    
+  }
+});

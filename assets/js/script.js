a2d7a06f203b22830cad26ce675fd482d180f782
FRANCESCO <OLIVOTTI>
2021-01-11 06:52:07 +0100

    esercizio + tentativo di bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
 const app = new Vue({
   el: "#app",
   data: {
-    ListaMail: []
+    MailList: [],
+    GridNumbers: [],
   },
+   methods: {
+        random(){
+            this.GridNumbers = [];
+            for (let i = 0; i < 36; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/int').then(response => {
+                    //console.log(response);
+                    //console.log(response.data.response);
+                    this.GridNumbers.push(response.data.response);
+                })                
+            }
+            console.log(this.GridNumbers);
+        }
+    },
   mounted: function() { 
     for (let i = 0; i < 10; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(risposta => {
-        this.ListaMail.push(risposta.data.response)
+      .then(response => {
+        this.MailList.push(response.data.response)
      });
     }
     
